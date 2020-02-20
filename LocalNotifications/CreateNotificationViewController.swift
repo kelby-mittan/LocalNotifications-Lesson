45817b2d9207ded965669e3bef18eaf7ed32fd14
Kelby Mittan <kelbymittan@pursuit.org>
2020-02-20 12:54:57 -0500

    able to get notifications

--- LocalNotifications/CreateNotificationViewController.swift
+++ LocalNotifications/CreateNotificationViewController.swift
@@ -21,16 +21,68 @@ class CreateNotificationViewController: UIViewController {
     
     weak var delegate: CreateNotificationControllerDelegate?
     
+    private var timeInterval: TimeInterval = Date().timeIntervalSinceNow + 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        
+        
+    }
+    
+    private func createLocalNotification() {
+        // step 1:
+        let content = UNMutableNotificationContent()
+        content.title = titleTextField.text ?? "No Title Given."
+        content.body = "Local Notifications is awesome"
+        content.subtitle = "Learning Local Notifications"
+        content.sound = .default
+        
+        // TODO: userInfo Dictionary
+//        content.userInfo =
+        
+        // step 2: create identifier
+        let identifier = UUID().uuidString // unique string
+        
+        // attatchment
+        if let imageURL = Bundle.main.url(forResource: "swift-logo", withExtension: "png") {
+            do {
+                let attatchment = try UNNotificationAttachment(identifier: identifier, url: imageURL, options: nil)
+                content.attachments = [attatchment]
+            } catch {
+                print("error getting attatchment: \(error)")
+            }
+        } else {
+            print("image resource could not be found")
+        }
+        
+        // create trigger - time interval, calendar, location
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: false)
+        
+        // create request
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        // add request to UNNotificationCenter
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("error adding request: \(error)")
+            } else {
+                print("request was successfully added")
+            }
+        }
+        
     }
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        guard sender.date > Date() else { return }
+        
+        timeInterval = sender.date.timeIntervalSinceNow
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        createLocalNotification()
         delegate?.didCreateNotification(self)
+        dismiss(animated: true)
     }
 }
+
+
--- LocalNotifications/NotificationsViewController.swift
+++ LocalNotifications/NotificationsViewController.swift
@@ -13,23 +13,51 @@ class NotificationsViewController: UIViewController {
 
     @IBOutlet var tableView: UITableView!
     
-    private var notifications = [UNNotificationRequest]()
+    private var notifications = [UNNotificationRequest]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
     
     private let center = UNUserNotificationCenter.current()
     
     private let pendingNotification = PendingNotification()
     
+    private var refreshControl: UIRefreshControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         tableView.dataSource = self
+        configureRefreshControl()
         checkForNotificationAuthorization()
         loadNotifications()
+        center.delegate = self
+    }
+    
+    private func configureRefreshControl() {
+        refreshControl = UIRefreshControl()
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(loadNotifications), for: .valueChanged)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navController = segue.destination as? UINavigationController, let createVC = navController.viewControllers.first as? CreateNotificationViewController else {
+            fatalError("could not downcast")
+        }
+        createVC.delegate = self
     }
 
-    private func loadNotifications() {
+    @objc private func loadNotifications() {
         pendingNotification.getPendingNotifications { (requests) in
             self.notifications = requests
+            
+            // stop the refresh control from animating and remove from the UI
+            DispatchQueue.main.async {
+                self.refreshControl.endRefreshing()
+            }
         }
     }
     
@@ -66,7 +94,26 @@ extension NotificationsViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "notificationCell", for: indexPath)
+        
+        let notification = notifications[indexPath.row]
+        cell.textLabel?.text = notification.content.title
+        cell.detailTextLabel?.text = notification.content.body
         return cell
+    }
+    
+}
+
+extension NotificationsViewController: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler(.alert)
+    }
+}
+
+extension NotificationsViewController: CreateNotificationControllerDelegate {
+    
+    func didCreateNotification(_ createNotificationController: CreateNotificationViewController) {
+        loadNotifications()
     }
     
     
